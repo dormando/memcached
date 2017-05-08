5a378c4c813cd0be111dbbfbf3ca3d7d37a13c2d
dormando <dormando@rydia.net>
2017-05-08 15:17:23 -0700

    per-LRU hits breakdown
    needs some more work: can de-dupe counter bump for slab stats get_hits.
    documentation
    dedupe some code in the aggregator
    specialized thread_stats_agg call to just aggregate the lru_hits

--- memcached.c
+++ memcached.c
@@ -1500,6 +1500,7 @@ static void process_bin_get_or_touch(conn *c) {
         } else {
             c->thread->stats.get_cmds++;
             c->thread->stats.slab_stats[ITEM_clsid(it)].get_hits++;
+            c->thread->stats.lru_hits[it->slabs_clsid]++;
         }
         pthread_mutex_unlock(&c->thread->stats.mutex);
 
@@ -3396,6 +3397,7 @@ static inline void process_get_command(conn *c, token_t *tokens, size_t ntokens,
                 /* item_get() has incremented it->refcount for us */
                 pthread_mutex_lock(&c->thread->stats.mutex);
                 c->thread->stats.slab_stats[ITEM_clsid(it)].get_hits++;
+                c->thread->stats.lru_hits[it->slabs_clsid]++;
                 c->thread->stats.get_cmds++;
                 pthread_mutex_unlock(&c->thread->stats.mutex);
                 *(c->ilist + i) = it;
